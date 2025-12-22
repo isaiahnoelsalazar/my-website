dfd236d36df2f3ce09576eba812455cd149f146b
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2025-12-22 21:41:13 +0800

    update: added IAmStillAlive project link to resources

--- links.js
+++ links.js
@@ -70,8 +70,9 @@ let csharp_programs = [
     ["https://github.com/isaiahnoelsalazar/WebBrowser/", "Web Browser", "A web browser made in C#"]
 ];
 let python_programs = [
-    ["https://github.com/isaiahnoelsalazar/CubeCubeCubeCube/", "Sample Python OpenGL - Cube cube cube cube | School Work", "A Python PyGame program that displays 4 rotating cubes with varying sizes using OpenGL", ["https://i.ibb.co/MDG0Tnvb/Screenshot-2025-10-26-210527.png"]],
+    ["https://isaiahnoelsalazar.github.io/IAmStillAlive/resources/build/web/index.html/", "IAmStillAlive", "A PyGame program in the web"],
     ["https://github.com/isaiahnoelsalazar/ManimGenerator/", "Manim Generator", "A Python program that generates simple animations using Manim"],
+    ["https://github.com/isaiahnoelsalazar/CubeCubeCubeCube/", "Sample Python OpenGL - Cube cube cube cube | School Work", "A Python PyGame program that displays 4 rotating cubes with varying sizes using OpenGL", ["https://i.ibb.co/MDG0Tnvb/Screenshot-2025-10-26-210527.png"]],
     ["https://github.com/isaiahnoelsalazar/SamplePythonOpenGLCube/", "Sample Python OpenGL - Cube | School Work", "A Python PyGame program that displays a rotating cube using OpenGL", ["https://i.ibb.co/dwtn1yLW/pythonopengl-cube.png"]],
     ["https://github.com/isaiahnoelsalazar/SamplePythonOpenGLCubeWithKeyInput/", "Sample Python OpenGL - Cube with Key Input | School Work", "A Python PyGame program that displays a rotating cube using OpenGL and accepts input events to manipulate the cube's position"],
     ["https://github.com/isaiahnoelsalazar/SamplePythonOpenGLDiamondWithKeyInput/", "Sample Python OpenGL - Diamond with Key Input | School Work", "A Python PyGame program that displays a rotating diamond using OpenGL and accepts input events to manipulate the diamond's position"],
